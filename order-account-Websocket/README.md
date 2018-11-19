20b3d3e6bddd2f997a6ab4534e7c150b2e27e4bb
libingnan@huobi.com <libingnan@huobi.com>
2018-11-19 15:38:54 +0800

    初始化 ng-gateway 的websocket demo

---
+++ order-account-Websocket/README.md
@@ -0,0 +1,26 @@
+# README
+## 环境要求
+nodejs 6.0以上版本
+
+## Demo说明
+ws.js
+演示使用websocket获取深度和K线数据
+
+## 使用指南
+```
+1.npm  install
+2.node ws.js
+```
+
+## 配置参数
+| 名称| 说明|
+|----|----|
+|accessKey |用户accessKey，需要修改|
+|secretKey | 用户secretKey，需要修改|
+
+
+## 联系作者
+magicdlf (QQ:1091472931)
+最新demo代码:
+https://github.com/magicdlf/huobipro
+
---
+++ order-account-Websocket/ws.js
@@ -0,0 +1,171 @@
+const moment = require('moment');
+const WebSocket = require('ws');
+const pako = require('pako');
+const CryptoJS = require ('crypto-js')
+
+const WS_URL = 'wss://api.huobi.pro/ws/v1';
+
+const host = "api.huobi.pro";
+const uri = "/ws/v1"
+
+
+// 修改您的accessKey 和 secretKey
+const config = {
+    accessKey: "7fc44160-1ccf05b5-f9b3e155-64215",
+    secretKey: "9efec8cf-1efe16b4-93783924-09044"
+}
+
+
+/**
+ * 签名计算
+ * @param method
+ * @param host
+ * @param path
+ * @param data
+ * @returns {*|string}
+ */
+function sign_sha(method, host, path, data) {
+    var pars = [];
+
+    //将参数值 encode
+    for (let item in data) {
+        pars.push(item + "=" + encodeURIComponent(data[item]));
+    }
+
+    //排序 并加入&连接
+    var p = pars.sort().join("&");
+
+    // 在method, host, path 后加入\n
+    var meta = [method, host, path, p].join('\n');
+
+    //用HmacSHA256 进行加密
+    var hash = CryptoJS.HmacSHA256(meta, config.secretKey);
+    // 按Base64 编码 字符串
+    var Signature = CryptoJS.enc.Base64.stringify(hash);
+    // console.log(p);
+    return Signature;
+}
+
+/**
+ * 发送unsub
+ * @param ws
+ */
+function unsub(ws) {
+    var data ={
+        op:"unsub",
+        cid:"111",
+        topic:"accounts"
+
+    }
+
+    ws.send(JSON.stringify(data));
+
+}
+
+/**
+ * 发送sub
+ * @param ws
+ */
+function sub(ws) {
+
+    var data ={
+        op:"sub",
+        cid:"111",
+        topic:"accounts"
+
+    }
+
+    ws.send(JSON.stringify(data));
+
+}
+
+/**
+ * 发送req
+ * @param ws
+ */
+function req(ws) {
+
+    var data ={
+        op:"req",
+        cid:"111",
+        topic:"orders.list"
+    }
+
+    ws.send(JSON.stringify(data));
+}
+
+/**
+ * 发送auth请求
+ * @param ws
+ */
+function auth(ws) {
+
+    const timestamp = moment.utc().format('YYYY-MM-DDTHH:mm:ss');
+
+    var data = {
+        AccessKeyId: config.accessKey,
+        SignatureMethod: "HmacSHA256",
+        SignatureVersion: "2",
+        Timestamp: timestamp,
+    }
+
+    //计算签名
+    sign_sha('GET', host, uri, data);
+    data["op"]="auth";
+    console.log(data);
+    ws.send(JSON.stringify(data));
+}
+
+
+function init() {
+    var ws = new WebSocket(WS_URL);
+    ws.on('open', () => {
+        console.log('open');
+        auth(ws);
+    });
+    ws.on('message', (data) => {
+        let text = pako.inflate(data, {
+            to: 'string'
+        });
+
+        let msg = JSON.parse(text);
+
+        if (  msg["err-code"] && msg["err-code"] !=0 ) {
+            //TODO 发生错误，可进行自定义处理
+            console.error(msg);
+        }
+
+        if (msg.op=="auth"){
+            console.log(msg)
+            sub(ws);//TODO  发送sub请求
+        }else if (msg.op == "ping") {
+
+            var pong ={
+                op: "pong",
+                ts: msg.ts
+            };
+            console.log(msg);
+            console.log(pong);
+            // 维持 ping pong
+            ws.send(JSON.stringify(pong));
+        }else if (msg.op=""){
+            // TODO 接收信息 进行业务处理
+            console.log(msg)
+        }
+
+
+    });
+    ws.on('close', () => {
+        // websocket连接关闭处理
+        console.log('close');
+        init();
+    });
+
+    ws.on('error', err => {
+        // websocket连接关闭处理
+        console.log('error', err);
+        init();
+    });
+}
+
+init();
